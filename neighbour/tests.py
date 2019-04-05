a03f2348e587106ffdbaf7511e476d12979ddf6f
CarineIzere <carizeree@gmail.com>
2019-04-05 17:01:30 +0200

    add test

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -26,6 +26,44 @@ class TestUser(TestCase):
     def test_save_profile(self):
         self.fuser = User(username="fuser", email="carineuser@mail.com")
         self.fuser.save()
+class ProfileTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_profile =Profile()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_method(self):
+        """
+        Function to test that profile is being saved
+        """
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+
+
+class ProjecTestClass(TestCase):
+    """  
+    Tests Project class and its functions
+    """
+    def setUp(self):
+        self.project = Project()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_method(self):
+        """
+        Function to test that a project is being saved
+        """
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+
 
 
       
